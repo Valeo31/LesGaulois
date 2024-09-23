2656f1ee2a6ead6596809bdcc770abb8ff460f5b
Valentin Leonard--bertrand <lnv5091a@ensfsi.onmicrosoft.com>
2024-09-23 19:40:34 +0200

    TP2 fini

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,8 +1,12 @@
 package personnages;
 
+import personnages.equipement.Equipement;
+
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement [] equipement = new Equipement[2];
+	private int nbEquipement = 0;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
@@ -31,7 +35,30 @@ public class Romain {
 		} else {
 			parler("J'abandonne...");
 		}
-		assert force > oldForce;
+		assert force < oldForce;
+	}
+	
+	private void reEquiper(Equipement eq) {
+		this.equipement[this.nbEquipement++]= eq;
+		System.out.println("Le soldat " + this.nom +" s'équipe avec un " + eq);
+	}
+	
+	private void sEquiper(Equipement eq) {
+		switch(this.nbEquipement) {
+		case 2:
+			System.out.println("Le soldat " + this.nom + " est déjà bien protégé");
+			break;
+		case 1:
+			if (eq == this.equipement[0]) {
+				System.out.println("Le soldat " + this.nom + " possède déjà un " + eq);
+			} else {
+				reEquiper(eq);
+			}
+			break;
+		case 0:
+			reEquiper(eq);
+			break;
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -40,7 +67,9 @@ public class Romain {
 		rom.prendreParole();
 		rom.parler("Bonjour je suis moi");
 		rom.recevoirCoup(7);
-		
-		
+		rom.sEquiper(Equipement.CASQUE);
+		rom.sEquiper(Equipement.CASQUE);
+		rom.sEquiper(Equipement.BOUCLIER);
+		rom.sEquiper(Equipement.CASQUE);
 	}
 }
